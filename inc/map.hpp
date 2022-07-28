19ec0c7027784784511ecd99d01411d2cc4b6020
Alf Jonathan Langhans <alanghan@student.42heilbronn.de>
2022-07-28 15:39:40 +0200

    Add insert functions and operator[] to map

--- inc/map.hpp
+++ inc/map.hpp
@@ -255,46 +255,38 @@ namespace ft
         return ( this->tree.max_size() );
     }
 
+    // https://cplusplus.com/reference/map/map/operator[]/
+    // Access element
+    // If k matches the key of an element in the container, the function returns a reference to its mapped value.
+    // If k does not match the key of any element in the container, the function inserts a new element with that key and returns a reference to its mapped value.
+    // Notice that this always increases the container size by one, even if no mapped value is assigned to the element (the element is constructed using its default constructor).
+    // A similar member function, map::at, has the same behavior when an element with the key exists, but throws an exception when it does not.
+    // A call to this function is equivalent to:
+    // (*((this->insert(make_pair(k,mapped_type()))).first)).second
     template <typename Key, typename T, typename Compare, typename Alloc>
     typename map<Key,T,Compare,Alloc>::mapped_type& map<Key,T,Compare,Alloc>::operator[]( const key_type& k )
     {
-        // code...
-
-        // https://cplusplus.com/reference/map/map/operator[]/
-        // Access element
-        // If k matches the key of an element in the container, the function returns a reference to its mapped value.
-        // If k does not match the key of any element in the container, the function inserts a new element with that key and returns a reference to its mapped value. Notice that this always increases the container size by one, even if no mapped value is assigned to the element (the element is constructed using its default constructor).
-        // A similar member function, map::at, has the same behavior when an element with the key exists, but throws an exception when it does not.
-        // A call to this function is equivalent to:
-        // (*((this->insert(make_pair(k,mapped_type()))).first)).second 
+        iterator position = this->insert( ft::make_pair( k, mapped_type() ).first );
+        return ( *( position )->second );
     }
 
     template <typename Key, typename T, typename Compare, typename Alloc>
     ft::pair<typename map<Key,T,Compare,Alloc>::iterator,bool> map<Key,T,Compare,Alloc>::insert( const value_type& val ) // single element
     {
-        // code...
-
-        // https://cplusplus.com/reference/map/map/insert/
-        // Insert elements
-        // Extends the container by inserting new elements, effectively increasing the container size by the number of elements inserted.
-        // Because element keys in a map are unique, the insertion operation checks whether each inserted element has a key equivalent to the one of an element already in the container, and if so, the element is not inserted, returning an iterator to this existing element (if the function returns a value).
-        // For a similar container allowing for duplicate elements, see multimap.
-        // An alternative way to insert elements in a map is by using member function map::operator[].
-        // Internally, map containers keep all their elements sorted by their key following the criterion specified by its comparison object. The elements are always inserted in its respective position following this ordering.
-        // The parameters determine how many elements are inserted and to which values they are initialized:
+        return ( this->tree.insert( val ) );
     }
     
     template <typename Key, typename T, typename Compare, typename Alloc>
     typename map<Key,T,Compare,Alloc>::iterator map<Key,T,Compare,Alloc>::insert( iterator position, const value_type& val ) // with hint
     {
-        // code...
+        return ( this->tree.insert( position, val ) );
     }
     
     template <typename Key, typename T, typename Compare, typename Alloc>
     template <typename InputIterator>
     void map<Key,T,Compare,Alloc>::insert( InputIterator first, InputIterator last ) // range
     {
-        // code...
+        this->tree.insert( first, last );
     }
     
     template <typename Key, typename T, typename Compare, typename Alloc>
